878391aa5b4fa1d27554fd5ba38a5e6f85f8aca1
khanh22010449 <22010449@st.phenikaa-uni.edu.vn>
2025-10-30 04:31:19 +0700

    update review

--- review.py
+++ review.py
@@ -174,7 +174,7 @@ def train_br(train_df, val_df, text_col, aspect_cols, output_dir, random_state=4
         if len(pos_idx) >= 5:
             X_train_pos = X_train[pos_idx]
             y_train_sent = y_train_raw.iloc[pos_idx].astype(int)
-            sentiment_clf = LogisticRegression(max_iter=1500, solver='saga', multi_class='multinomial', C=0.1)
+            sentiment_clf = LogisticRegression(max_iter=1500, solver='saga', multi_class='multinomial', C=0.3)
             sentiment_clf.fit(X_train_pos, y_train_sent)
             joblib.dump(sentiment_clf, os.path.join(output_dir, f'sentiment_{aspect}.joblib'))
 
